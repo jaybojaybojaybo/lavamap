e7a836d44a4f31df023764f48ca92be295858a9e
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-02-04 17:49:53 -0800

    add data sharing to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>  
-  <router-view/>
+  <navbar id="navbar" @setShape="provideShape"></navbar>
+  <router-view />
 </template>
 
 <script lang="ts">
-import { onMounted } from "vue";
+import { defineComponent, onMounted, provide, ref, Ref } from "vue";
+import Navbar from '@/components/Navbar.vue'
 
-export default {
+export default defineComponent({
+  components: {
+    Navbar
+  },
   setup() {
+    function provideShape(val: Ref) {
+      console.log('val on App: ', val)
+      provide('selectedShape', val)
+    }
     onMounted(() => {
       console.log('we out here')
+      provideShape(ref('Cube'))
     })
+    return {
+      provideShape      
+    }
   }
-}
+})
 </script>
 
 <style>
