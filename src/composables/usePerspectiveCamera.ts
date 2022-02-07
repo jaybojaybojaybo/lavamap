c12e02e55959f9b456d70cafd86383489fbb1395
jaybojaybojaybo <jasun.feddema@gmail.com>
2022-02-06 23:54:10 -0800

    adjust init position, remove layers

--- src/composables/usePerspectiveCamera.ts
+++ src/composables/usePerspectiveCamera.ts
@@ -4,19 +4,15 @@ export default function usePerspectiveCamera(gridSize: number, aspectRatio: numb
     /* CAMERA - Perspective (cameraPersp = Primary) */
     const fov = 50 // Field of view --- lower to reduce distortion    
     const near = 0.1 // Near clipping plane
-    const far = 1000 // Far clipping plane
+    const far = gridSize * 2 // Far clipping plane
     const perspCamera = new PerspectiveCamera(fov, aspectRatio, near, far)
     
     // starting position
-    const z = Math.tan(30 * Math.PI/180) * (gridSize/ 10)
-    const y = Math.tan(20 * Math.PI/180) * z
+    const z = Math.tan(30 * Math.PI/180) * (gridSize / 10)
+    const y = Math.tan(60 * Math.PI/180) * z
     perspCamera.position.set(0, y, z)
     perspCamera.lookAt(0, 0 ,0)
     perspCamera.updateProjectionMatrix()
-    perspCamera.layers.enable(0)
-    perspCamera.layers.enable(1)
-    perspCamera.layers.enable(2)
-    
     perspCamera.name = "perspCamera"
     
     return perspCamera
